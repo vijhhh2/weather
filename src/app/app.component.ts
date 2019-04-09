805e0e9bddccc13f94b515dd21e74c524d6b8412
vijay kumar <vij123456789@gmail.com>
2019-04-09 10:55:57 +0530

    bare bones structer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Minimus';
+  showMenu: false;
+  darkModeActive: false;
+  toggleMenu() {}
+  modeToggleSwitch() {}
 }
